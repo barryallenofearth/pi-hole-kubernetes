9f483905128614d02e92b3c05ee0cc5c401b507a
Michael <lordrevan1999@web.de>
2025-01-29 22:19:48 +0100

    migrate to hostPath volumeClaims

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -6,6 +6,13 @@ alias kubectl='microk8s kubectl'
 echo "configure base project definitions"
 kubectl apply -f $KUBERNETES_CONFIGS_PATH
 
+echo "create host paths"
+sudo mkdir -p /etc/pihole/server1
+sudo mkdir -p /etc/pihole/server2
+sudo mkdir -p /etc/dnsmasq.d/server1
+sudo mkdir -p /etc/dnsmasq.d/server2
+
+
 read -p -r "Enter pi-hole admin password:" password
 
 echo "create admin password secret"
